f7ff6369eae6aefb0aef8ed6206e4c626677caa7
Simone <codarinsimone@gmail.com>
2023-11-30 19:19:57 +0100

    modifica codice e install uuid

--- src/components/ColorGrading.jsx
+++ src/components/ColorGrading.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Values from "values.js";
 import SingleColor from "./SingleColor";
 import { v4 as uuidv4 } from "uuid";
--- src/components/SingleColor.jsx
+++ src/components/SingleColor.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import {useState, useEffect} from "react";
 import {rgbToHex} from "../utils/helpers";
 
 const SingleColor = ({rgb, type, weight}) => {
